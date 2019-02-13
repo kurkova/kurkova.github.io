fe5e61d3e98ca3d529eed25bf707ccb041b7e5e4
kurkova <kurkiewiczpaula@gmail.com>
2019-02-13 16:58:27 +0000

    changed html,css,js file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://fast-caverns-49696.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://trello.com/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
